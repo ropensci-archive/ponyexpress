97f849f485c1c6db657555a707fd93d6cb49f24e
Karthik Ram <karthik.ram@gmail.com>
2017-05-26 17:22:58 -0700

    updated docs

--- R/parcel_send.R
+++ R/parcel_send.R
@@ -1,7 +1,7 @@
 #'  Sends email from notifier object
 #'
 #' @param secret path to secret.json file
-#' @param edat tibble outputted from note_create
+#' @param email_data tibble outputted from note_create
 #' @param type Type of email. Either text or html.
 #' @param sent Path for sent emails to be saved. You will need this later as a log of sent emails.
 #'
@@ -20,7 +20,7 @@
 #' # Now send emails with
 #' \dontrun{parcel_send(sample_email, secret)}
 parcel_send <-
-  function(edat,
+  function(email_data,
            type = "text",
            secret = NULL,
            sent = "sent-emails.rds") {
@@ -35,7 +35,7 @@ parcel_send <-
     body <- gsub("\n", "<br>", body)
     gmailr::html_body(z, body = body)
     }
-    emails <- purrr::pmap(edat, mime2)
+    emails <- purrr::pmap(email_data, mime2)
     safe_send_message <- purrr::safely(gmailr::send_message)
     sent_mail <- purrr::map(emails, safe_send_message)
     error_list <- gmail_errors(sent_mail)
